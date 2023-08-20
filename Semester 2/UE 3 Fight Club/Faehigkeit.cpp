e69b3ba44a5c207d7c4eaf78a2f6f22f12b5c702
Alexander Nachtmann <124206820+ANcpLua@users.noreply.github.com>
2023-08-20 22:15:31 +0200

    Delete Semester 2/UE 3 Fight Club directory

--- Semester 2/UE 3 Fight Club/Faehigkeit.cpp
+++
@@ -1,6 +0,0 @@
-#include "Faehigkeit.h"
-
-Faehigkeit::Faehigkeit(std::string name, int schaden)
-        : name(name), schaden(schaden)
-{
-}
--- Semester 2/UE 3 Fight Club/character_builder.cpp
+++
@@ -1,6 +0,0 @@
-#include "character_builder.h"
-
-Charakter CharacterBuilder::erstelleCharakter(const std::string &name, const std::string &element, const std::vector<Faehigkeit> &charakterFaehigkeiten)
-{
-    return Charakter(name, 1000, element, charakterFaehigkeiten);
-}
