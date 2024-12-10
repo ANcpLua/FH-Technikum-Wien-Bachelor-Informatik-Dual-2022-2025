45076c8302684e5862e2d9f51b5a1ca40708540a
Alexander Nachtmann <anfh22@outlook.com>
2024-12-10 23:25:20 +0100

    Delete Semester 1/C - Proz/Prozedurale-Sprachen-BIF-DUA-1-WS2022-PROZD-DE_1-Labor-C-Alle-Selbstchecks-main directory

--- Semester 1/C - Proz/Prozedurale-Sprachen-BIF-DUA-1-WS2022-PROZD-DE_1-Labor-C-Alle-Selbstchecks-main/B Variablen, Ein- und Ausgabe/Selbstcheck - Ausgabe von Werten.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main()
-{
-    printf("35*42=%d\n", 35*42);
-    printf("3.5*4.2=%f\n", 3.5*4.2);
-    return 0;
-}
--- Semester 1/C - Proz/Prozedurale-Sprachen-BIF-DUA-1-WS2022-PROZD-DE_1-Labor-C-Alle-Selbstchecks-main/E Schleifen/Selbstcheck - for-Schleife1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    for (int i = 1; i < 21; i++) {
-        printf("%d ", i);
-    }
-    return 0;
-}
