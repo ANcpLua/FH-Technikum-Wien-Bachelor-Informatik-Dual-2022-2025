7e4e24963c43b7939f34af012b8dbcab965fff82
Alexander Nachtmann <anfh22@outlook.com>
2025-02-24 03:27:33 +0100

    Add files via upload

---
+++ Semester 4/SWEN2 C#/UI/wwwroot/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'tourplanner-cache-v1';
+
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/css/app.css',
+    '/css/bootstrap/bootstrap.min.css',
+    '/_framework/blazor.webassembly.js',
+    '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
